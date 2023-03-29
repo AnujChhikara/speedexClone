2c66af2f6ba74bbc2106706ff6d37d5a73ad00a2
AnujChhikara <anujchhikara777@gmail.com>
2023-03-29 21:28:33 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      rotate: {
+        60: "15deg",
+      },
+    },
+  },
+  plugins: [],
+};
