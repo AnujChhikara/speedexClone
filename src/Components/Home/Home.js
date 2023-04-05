21d92a52209056c891eb7cd4d5042fc2d42ad665
AnujChhikara <anujchhikara777@gmail.com>
2023-04-05 21:13:17 +0530

    heading

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -83,7 +83,7 @@ function Home() {
         <div className="flex justify-center w-44 align -rotate-60">
           <img className="h-[440px]" src="/Images/b.webp" alt="" />
         </div>
-        <div className="w-[600px] text-5xl relative mt-40">
+        <div className="w-[660px] text-5xl font-medium flex ml-40 items-start relative mt-40">
           <h1>
             Join the Reusable Revolution with Speedex's Eco-Friendly Water
             Bottles.
